45a917343c62a2d903431202aabe3ba452ee9777
Becks <beckspoppn@pop-os.localdomain>
2020-07-16 18:25:34 -0400

    move fastify stuff to another repo

--- src/gen/server/fastify.ts
+++ src/gen/server/fastify.ts
@@ -1,3 +1,4 @@
+import {MethodSignature, ParameterDeclaration} from 'ts-morph'
 import {ServerGenerator} from '../generator'
 import {Parser} from '../parser'
 /**
@@ -18,7 +19,7 @@ export class FastifyGenerator extends ServerGenerator {
 /* eslint-disable new-cap */
 import {FastifyPluginAsync, LogLevel} from 'fastify'
 import fp, {PluginOptions} from 'fastify-plugin'
-import {pluginOpts, registerOptions, TypeRpcPlugin} from './rpc.server.util'`
+import {pluginOpts, registerOptions, TypeRpcPlugin} from './rpc.server.util'\n`
   }
 
   private util() {
@@ -45,12 +46,43 @@ export const registerOptions = (prefix: string, logLevel: LogLevel): RegisterOpt
 `
   }
 
+  // generates the RequestGenericInterface parameter for each route
+  // see https://www.fastify.io/docs/latest/TypeScript/#using-generics
+  protected getIncomingMessageType(method: MethodSignature): string {
+    const params = this.parser.getParams(method)
+    switch (params.length) {
+    case 0:
+      return '{Body: {}}'
+
+    case 1:
+      return `{Body: {${params[0]}}}`
+
+    default:
+      return `{Body: {${this.buildBody(params)}}}`
+    }
+  }
+
+  private buildBody(params: ParameterDeclaration[]): string {
+    let body = ''
+    params.forEach(param => {
+      body += `${param}\n`
+    })
+    return body
+  }
+
   private utilsFile(): [string, string] {
     return ['rpc.server.util.ts', this.util()]
   }
 
+  generateFile(): string {
+    return `${this.imports()}`
+  }
+
   async generate(): Promise<Map<string, string>> {
-    throw new Error('Method not implemented.')
+    const code = new Map<string, string>()
+    const util = this.utilsFile()
+    code.set(util[0], util[1])
+    return code
   }
 }
 
