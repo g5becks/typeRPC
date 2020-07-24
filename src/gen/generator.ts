11b5d847fdff3967e08d932e16ef8487d6f65c0f
Gary Becks <techstar.dev@hotmail.com>
2020-07-24 19:14:17 -0400

    got fast

--- src/gen/generator.ts
+++ src/gen/generator.ts
@@ -173,9 +173,18 @@ export const ${type}Schema = ${schema}\n
   }
 
   protected buildStringifyFuncForType(type: string, schema: string): string {
+    const parsed = JSON.parse(schema)
+    // eslint-disable-next-line no-console
+    console.log(parsed)
+    const gotten = parsed.definitions[type]
+    const json = JSON.stringify(gotten)
+    // eslint-disable-next-line no-console
+    console.log(`attempting to use ${type} to get data`)
+    // eslint-disable-next-line no-console
+    console.log(`got data ${json}`)
     return `
 const ${type}Stringify = fastJson(
-  ${schema}
+  ${json}
 )
     `
   }
