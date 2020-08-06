4fadd61442a4b7d0125588176a81c02a775fd263
Gary Becks <techstar.dev@hotmail.com>
2020-08-06 12:55:22 -0400

    change namespaces in types module to objects

--- src/schema/types.ts
+++ src/schema/types.ts
@@ -12,24 +12,39 @@ export namespace containers {
   export const Struct: Struct = {} as unknown as Struct
 }
 
-export namespace primitives {
-  export const Bool: t.Bool = {} as unknown as t.Bool
-  export const Int8: t.Int8 = {} as unknown as t.Int8
-  export const Uint8: t.Uint8 = {} as unknown as t.Uint8
-  export const Int16: t.Int16 = {} as unknown as t.Int16
-  export const Uint16: t.Uint16 =  {} as unknown as t.Uint16
-  export const Int32: t.Int32 = {} as unknown as t.Int32
-  export const Uint32: t.Uint32 = {} as unknown as t.Uint32
-  export const Int64: t.Int64 = {} as unknown as t.Int64
-  export const Uint64: t.Uint64 = {} as unknown as t.Uint64
-  export const Float32: t.Float32 = {} as unknown as t.Float32
-  export const Float64: t.Float64 = {} as unknown as t.Float64
-  export const Null: t.Null = {} as unknown as t.Null
-  export const String: t.String = {} as unknown as t.String
-  export const Error: t.Err = {} as unknown as t.Err
-  export const Any: t.Any = {} as unknown as t.Any
-  export const TimeStamp: t.TimeStamp = {} as unknown as t.TimeStamp
-  export const Blob: t.Blob = {} as unknown as t.Blob
+export const make = {
+  Struct: (name: string): Struct => { return {name} as Struct },
+
+  Dict: (keyType: t.Comparable, valType: VarType): t.Dict => {
+    return {keyType, valType} as t.Dict
+  },
+  Tuple2: (item1: VarType, item2: VarType): t.Tuple2 => {
+    return {item1,item2} as t.Tuple2
+  },
+  Tuple3: (item1: VarType, item2: VarType, item3: VarType): t.Tuple3 => {
+    return {item1,item2,item3} as t.Tuple3
+  }
+}
+
+
+export const primitives = {
+  Bool: {_type_: 'Bool'} as unknown as t.Bool,
+  Int8: {_type_: 'Int8'} as unknown as t.Int8,
+  Uint8: {_type_: 'Uint8'} as unknown as t.Uint8,
+  Int16: {_type_: 'Int16'} as unknown as t.Int16,
+  Uint16: {_type_: 'Uint16'} as unknown as t.Uint16,
+  Int32: {_type_: 'Int32'} as unknown as t.Int32,
+  Uint32: {_type_: 'Uint32'} as unknown as t.Uint32,
+  Int64: {_type_: 'Int64'} as unknown as t.Int64,
+  Uint64: {_type_: 'Uint64'} as unknown as t.Uint64,
+  Float32: {_type_: 'Float32'} as unknown as t.Float32,
+  Float64: {_type_: 'Float64'} as unknown as t.Float64,
+  Null: {_type_: 'Null'} as unknown as t.Null,
+  String: {_type_: 'String'} as unknown as t.String,
+  Error: {_type_: 'Err'} as unknown as t.Err,
+  Any: {_type_: 'Any'} as unknown as t.Any,
+  TimeStamp: {_type_: 'TimeStamp'} as unknown as t.TimeStamp,
+  Blob: {_type_: 'Blob'} as unknown as t.Blob,
 }
 
 
@@ -47,12 +62,17 @@ export class OptionalParam {
   }
 }
 
-export const isDict = (type: VarType): type is t.Dict => {
-  const props = Object.getOwnPropertyNames(type)
-  return props.includes('keyType') && props.includes('valueType')
+export const is = {
+  Dict = (type: VarType): type is t.Dict => verifyType(type,'keyType', 'valueType')
 }
 
-export const isTuple2 = (type: VarType): type is t.Tuple2 => {
-  const props = Object.getOwnPropertyNames(type)
-  return props.includes('item1') && props.includes('item2')
+const verifyType = (type: VarType, ...props: string[]): boolean => {
+  const names = Object.getOwnPropertyNames(type)
+  return props.every(prop => names.includes(prop))
 }
+
+export const isDict = (type: VarType): type is t.Dict => verifyType(type,'keyType', 'valueType')
+
+export const isTuple2 = (type: VarType): type is t.Tuple2 => verifyType(type, props => props.includes('item1') && props.includes('item2'))
+
+export const isTuple3 = (type: VarType): type is t.Tuple3 => verifyType(type, props => props.includes('item1') && props)
