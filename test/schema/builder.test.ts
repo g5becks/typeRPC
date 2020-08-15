f68b640eb284be264df042ccb80260c5f65b8223
Gary Becks <techstar.dev@hotmail.com>
2020-08-15 19:12:59 -0400

    add test for isCbor function

--- test/schema/builder.test.ts
+++ test/schema/builder.test.ts
@@ -1,7 +1,10 @@
 import {internalTesting} from '../../src/schema/builder'
-import {Project, SourceFile} from 'ts-morph'
+import {Project} from 'ts-morph'
 import {containersList, primitivesMap} from '../../src/schema/types'
-import * as faker from 'faker'
+// @ts-ignore
+import {getSourceFile, makeRandomType, randomNumber} from './util'
+import exp = require('constants')
+
 const {
   isType,
   isCbor,
@@ -27,65 +30,6 @@ beforeEach(() => {
   project = new Project()
 })
 
-function randomNumber(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min) + min)
-}
-
-const makePrimitiveType = () => {
-  const prims = [...primitivesMap.keys()]
-  return prims[randomNumber(0, prims.length - 1)]
-}
-const makeRandomVars = (count: number): string => {
-  let vars = ''
-  for (let i = 0; i < count; i++) {
-    vars = vars.concat(`var testVar${count}: ${makePrimitiveType()}`)
-  }
-  return vars
-}
-const comparables = ['t.bool', 't.int8', 't.uint8', 't.uint16', 't.int16', 't.int32', 't.uint32', 't.int64', 't.uint64', 't.float32', 't.float64', 't.str', 't.timestamp', 't.err', 't.dyn']
-
-const randomComparable = () => comparables[randomNumber(0, comparables.length - 1)]
-const randomContainer = (): string => {
-  const containers = [`t.Dict<${randomComparable()}, ${randomComparable()}>`, `t.List<${randomComparable()}>`, `t.Tuple2<${randomComparable()}, ${randomComparable()}>`, `t.Tuple3<${randomComparable()}, ${randomComparable()}, ${randomComparable()}>`, `t.Tuple4<${randomComparable()}, ${randomComparable()}, ${randomComparable()}, ${randomComparable()}>`, `t.Tuple5<${randomComparable()},${randomComparable()}, ${randomComparable()}, ${randomComparable()}, ${randomComparable()}>`]
-  return containers[randomNumber(0, containers.length - 1)]
-}
-
-const randomStructName =  (): string => faker.name.firstName().toUpperCase()
-const randomKeyable = () => {
-  const keyables = [randomComparable(), randomContainer(), randomStructName()]
-  return keyables[randomNumber(0, keyables.length - 1)]
-}
-const makeDict = () => `t.Dict<${randomComparable()}, ${randomKeyable()}>`
-
-const makeList = () => `t.List<${randomKeyable()}>`
-
-const makeTuple2 = () => `t.Tuple2<${randomKeyable()}, ${randomKeyable()}>`
-
-const makeTuple3 = () => `t.Tuple3<${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}>`
-
-const makeTuple4 = () => `t.Tuple4<${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}>`
-
-const makeTuple5 = () => `t.Tuple5<${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}>`
-
-const makeRandomDataType = (): string => {
-  const makers = [randomStructName, makeDict, makeList, makeTuple2, makeTuple3, makeTuple4, makeTuple5, randomComparable, () => 't.unit', () => 't.nil']
-  return makers[randomNumber(0, makers.length - 1)]()
-}
-
-const makeRandomType = (propCount: number): string => {
-  let props = ''
-  for (let i = 0; i < propCount; i++) {
-    props = props.concat(`prop${propCount}: ${makeRandomDataType()};\n`)
-  }
-  return `type TestType = {
-    ${props}
-  }`
-}
-
-const getSourceFile = (source: string, project: Project): SourceFile => {
-  project.createSourceFile('test.ts', source)
-  return project.getSourceFile('test.ts')!
-}
 test('isType() should return true when given the proper type', () => {
   let vars = ''
   const types = [...primitivesMap.keys(), ...containersList]
@@ -109,7 +53,7 @@ test('makeRandomType() should return type with correct number of props', () => {
 })
 
 test('makeDataType() should return correct DataType for type prop', () => {
-  const type = makeRandomType(randomNumber(140, 300))
+  const type = makeRandomType(randomNumber(200, 400))
   const file = getSourceFile(type, project)
   const types = file.getTypeAliases()[0].getTypeNode()!.forEachChildAsArray()
   for (const type of types) {
@@ -118,4 +62,16 @@ test('makeDataType() should return correct DataType for type prop', () => {
     console.log('created: ' + dataType.toString() + ' actual: ' + propType.getText().trim())
     expect(dataType.toString()).toEqual(propType.getText().trim())
   }
+})
+
+test('isCbor() should return true when jsDoc contains cbor string', () => {
+  const source = `
+/**
+* cbor
+*/
+type BinaryType = {
+  data: t.blob
+}
+`
+  expect(isCbor(getSourceFile(source, project).getTypeAliases()[0])).toBeTruthy()
 })
--- test/schema/util.ts
+++ test/schema/util.ts
@@ -1,3 +1,6 @@
+import {Project, SourceFile} from 'ts-morph'
+import {primitivesMap} from '../../src/schema/types'
+import * as faker from 'faker'
 export const validImport = 'import {t} from \'@typerpc/types\''
 export const validInterface = `
   interface Test {
@@ -9,3 +12,51 @@ ${validImport}
 ${source}
 ${validInterface}
 `
+export function randomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+const comparables = ['t.bool', 't.int8', 't.uint8', 't.uint16', 't.int16', 't.int32', 't.uint32', 't.int64', 't.uint64', 't.float32', 't.float64', 't.str', 't.timestamp', 't.err', 't.dyn']
+
+const randomComparable = () => comparables[randomNumber(0, comparables.length - 1)]
+const randomContainer = (): string => {
+  const containers = [`t.Dict<${randomComparable()}, ${randomComparable()}>`, `t.List<${randomComparable()}>`, `t.Tuple2<${randomComparable()}, ${randomComparable()}>`, `t.Tuple3<${randomComparable()}, ${randomComparable()}, ${randomComparable()}>`, `t.Tuple4<${randomComparable()}, ${randomComparable()}, ${randomComparable()}, ${randomComparable()}>`, `t.Tuple5<${randomComparable()},${randomComparable()}, ${randomComparable()}, ${randomComparable()}, ${randomComparable()}>`]
+  return containers[randomNumber(0, containers.length - 1)]
+}
+
+const randomStructName =  (): string => faker.name.firstName().toUpperCase()
+const randomKeyable = () => {
+  const keyables = [randomComparable(), randomContainer(), randomStructName()]
+  return keyables[randomNumber(0, keyables.length - 1)]
+}
+const makeDict = () => `t.Dict<${randomComparable()}, ${randomKeyable()}>`
+
+const makeList = () => `t.List<${randomKeyable()}>`
+
+const makeTuple2 = () => `t.Tuple2<${randomKeyable()}, ${randomKeyable()}>`
+
+const makeTuple3 = () => `t.Tuple3<${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}>`
+
+const makeTuple4 = () => `t.Tuple4<${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}>`
+
+const makeTuple5 = () => `t.Tuple5<${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}, ${randomKeyable()}>`
+
+const makeRandomDataType = (): string => {
+  const makers = [randomStructName, makeDict, makeList, makeTuple2, makeTuple3, makeTuple4, makeTuple5, randomComparable, () => 't.unit', () => 't.nil']
+  return makers[randomNumber(0, makers.length - 1)]()
+}
+
+export const makeRandomType = (propCount: number): string => {
+  let props = ''
+  for (let i = 0; i < propCount; i++) {
+    props = props.concat(`prop${propCount}: ${makeRandomDataType()};\n`)
+  }
+  return `type TestType = {
+    ${props}
+  }`
+}
+
+export const getSourceFile = (source: string, project: Project): SourceFile => {
+  project.createSourceFile('test.ts', source)
+  return project.getSourceFile('test.ts')!
+}
