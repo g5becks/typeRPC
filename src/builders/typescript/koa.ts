32c1e4148beab230a311f7dd42ffd2b9e7d15393
Gary Becks <techstar.dev@hotmail.com>
2020-08-18 09:30:47 -0400

    add buildMethod, and buildInterface methods

--- src/builders/typescript/koa.ts
+++ src/builders/typescript/koa.ts
@@ -1,16 +1,47 @@
 import {Code, CodeBuilder} from '..'
-import {Schema} from '../../schema'
-import {fileHeader} from '../utils'
-import {TypeDef} from '../../schema/schema'
+import {Method, Schema} from '../../schema'
+import {capitalize, fileHeader} from '../utils'
+import {Interface, TypeDef} from '../../schema'
+import {dataType} from './helpers'
 
+const handleOptional = (isOptional: boolean): string => isOptional ? '?' : ''
+const buildProps = (type: TypeDef): string => {
+  let props = ''
+  for (const prop of type.properties) {
+    props = props.concat(`${prop.name}${handleOptional(prop.isOptional)}: ${dataType(prop.type)}\n`)
+  }
+  return props
+}
 const buildType = (type: TypeDef): string => {
   return `
-type ${type.}=  `
+type ${capitalize(type.name)} = {
+  ${buildProps(type)}
+}\n`
+}
+
+const buildTypes = (schema: Schema): string => {
+  let types =  ''
+  for (const type of schema.types) {
+    types = types.concat(buildType(type))
+  }
+  return types
+}
+
+const buildMethod = (method: Method): string => {
+
+}
+const buildInterface = (interfc: Interface): string => {
+  return `
+interface ${capitalize(interfc.name)} {
+
+}\n`
 }
 const buildServer = (schema: Schema): Code => {
   const source = `
 import Router from '@koa/router'
-${fileHeader()}`
+${fileHeader()}
+${buildTypes(schema)}
+`
   return {fileName: schema.fileName + '.ts', source}
 }
 
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,6 @@
-import {Schema} from './schema'
+import {HTTPVerb, Interface, Method, Param, Property, Schema, TypeDef} from './schema'
 import {buildSchemas} from './builder'
 import {is} from './types'
 
-export {Schema, buildSchemas, is}
+export {Schema, buildSchemas, is, HTTPVerb, Interface, TypeDef, Param, Property, Method}
 
