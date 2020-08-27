fa7c3a305d27e36269613062f94a945b5ccbad43
Gary Becks <techstar.dev@hotmail.com>
2020-08-27 11:57:23 -0400

    remove HttpVerb from types export

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,6 @@
-import {HTTPVerb, Message, MutationMethod, Param, Property, Schema, QueryService, Import} from './schema'
+import {Message, MutationMethod, Param, Property, Schema, QueryService, Import} from './schema'
 import {DataType, is, make, Struct, StructLiteral, StructLiteralProp} from './types'
 import {buildSchemas} from './builder'
 
-export {buildSchemas, Schema, make, DataType, is, HTTPVerb, Import, QueryService, Message, Param, Property, MutationMethod, Struct, StructLiteral, StructLiteralProp}
+export {buildSchemas, Schema, make, DataType, is, Import, QueryService, Message, Param, Property, MutationMethod, Struct, StructLiteral, StructLiteralProp}
 
