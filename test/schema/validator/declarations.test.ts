83286a2be10360a63eff4a50c9570fbb5394593e
Gary Becks <techstar.dev@hotmail.com>
2020-08-28 12:57:11 -0400

    more validation tests

--- test/schema/validator/declarations.test.ts
+++ test/schema/validator/declarations.test.ts
@@ -29,20 +29,20 @@ const runTest = (source: string, validator: (file: SourceFile) => Error[]) => {
   expect(validator(genSourceFile(source, project)).length).toEqual(1)
 }
 
-test(testName('function'), () => {
+test(testName('function'), () =>
   runTest(`
   function name() {
   }
   `, validateFunctions,)
-})
+)
 
-test(testName('interface'), () => {
+test(testName('interface'), () =>
   runTest(`
   interface Name {
   }`, validateInterfaces)
-})
+)
 
-test(testName('multiple function'), () => {
+test(testName('multiple function'), () =>
   runTest(`
   function name() {
   }
@@ -50,7 +50,7 @@ test(testName('multiple function'), () => {
   function name2() {
   }
   `, validateFunctions)
-})
+)
 
 test(testName('variable declaration'), () => {
   runTest(`
@@ -63,9 +63,70 @@ test(testName('variable declaration'), () => {
   let you: number = 2`, validateVariables)
 })
 
-test(testName('class declaration'), () => {
+test(testName('class declaration'), () =>
   runTest(`
   class MyClass {
   private name: string = ''
   }`, validateClasses)
+)
+
+test(testName('interface'), () =>
+  runTest(`
+  export default interface {
+    name() : t.str;
+  }`, validateInterfaces)
+)
+
+test(testName('namespace'), () =>
+  runTest(`
+  namespace Cars {
+    export type Fake = string | boolean
+  }`, validateNameSpaces)
+)
+
+test(testName('for loop'), () =>
+  runTest(`
+  for (let q of [1,2,3]) {
+  }`, validateStatements)
+)
+
+test(testName('Enum'), () =>
+  runTest(`
+  enum Colors {
+    Red,
+    Blue,
+    Green,
+  }`, validateEnums)
+)
+
+test(testName('Generic type'), () => {
+  const typeName = 'GenericType<T, S, V>'
+  const source = `
+type ${typeName} = {
+  age: t.str;
+}
+  `
+  runTest(source, validateTypes)
 })
+
+test(testName('type alias with object literal property'), () =>
+  runTest(`
+  type MyType = {
+  name: t.str;
+  cars: {
+  model: t.str;
+  }
+  }`, validateTypes)
+)
+
+test(testName('empty rpc.Msg'), () =>
+  runTest(`
+  type TestType = rpm.Msg<{}>`, validateTypes)
+)
+
+test(testName('type alias with invalid type property'), () =>
+  runTest(`
+  type MyType = {
+    name: Name;
+  }
+  `, validateTypes))
