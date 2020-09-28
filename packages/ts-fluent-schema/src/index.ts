5a763a3503016831e1d1384bac830e1491802015
Gary Becks <techstar.dev@hotmail.com>
2020-09-28 16:41:26 -0400

    add request and response naming functions

--- packages/ts-fluent-schema/src/index.ts
+++ packages/ts-fluent-schema/src/index.ts
@@ -10,7 +10,6 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import { Code } from '@typerpc/plugin'
 import { DataType, is, make, Message, MutationMethod, QueryMethod, Schema, StructLiteral } from '@typerpc/schema'
 import { capitalize, lowerCase } from 'plugin-utils/dist'
 const typeMap: Map<string, string> = new Map<string, string>([
@@ -27,17 +26,19 @@ const typeMap: Map<string, string> = new Map<string, string>([
     [make.float64.type, 'number()'],
     [make.nil.type, 'null()'],
     [make.str.type, 'string()'],
-    [make.dyn.type, ''],
+    [make.dyn.type, 'object()'],
     // Math.round(Date.now() / 1000)
     [make.timestamp.type, 'number()'],
-    [make.unit.type, ''],
+    [make.unit.type, 'object()'],
     [make.blob.type, 'array().items(S.number())'],
 ])
 
 const buildObjectSchema = (struct: StructLiteral): string => {
     let obj = 'object()'
     for (const prop of struct.properties) {
-        obj = obj.concat(`.prop('${prop.name}', S.${schemaType(prop.type)}${prop.isOptional ? '' : '.required()'})`)
+        obj = obj.concat(
+            `.prop('${lowerCase(prop.name)}', S.${schemaType(prop.type)}${prop.isOptional ? '' : '.required()'})`,
+        )
     }
     return obj
 }
@@ -92,7 +93,7 @@ const buildMsgSchema = (msg: Message): string => {
     let schema = `S.object().id('${msg.name}').title('${msg.name} Schema').description('Schema for ${msg.name} rpc message')`
     for (const prop of msg.properties) {
         schema = schema.concat(
-            `.prop('${prop.name}', S.${schemaType(prop.type)})${prop.isOptional ? '' : '.required()'}`,
+            `.prop('${lowerCase(prop.name)}', S.${schemaType(prop.type)})${prop.isOptional ? '' : '.required()'}`,
         )
     }
     return `const ${lowerCase(msg.name)}Schema = ${buildMsgSchema}
@@ -107,7 +108,7 @@ const buildMsgSchemasForFile = (schema: Schema): string => {
     return msgs
 }
 
-const buildRequestSchema = (svcName: string, method: MutationMethod | QueryMethod): string => {
+const buildRequestSchema = (svcName: string, method: MutationMethod): string => {
     let schema = `S.object().id('${svcName}.${method.name}Request').title('${svcName}.${method.name} Body').description('${svcName}.${method.name} Request Schema')`
     for (const param of method.params) {
         schema = schema.concat(`.prop('${param.name}', ${schemaType(param.type)})`)
@@ -118,11 +119,6 @@ const buildRequestSchema = (svcName: string, method: MutationMethod | QueryMetho
 
 const buildRequestSchemasForFile = (schema: Schema): string => {
     let schemas = ''
-    for (const svc of schema.queryServices) {
-        for (const method of svc.methods) {
-            schemas = schemas.concat(buildRequestSchema(svc.name, method))
-        }
-    }
     for (const svc of schema.mutationServices) {
         for (const method of svc.methods) {
             schemas = schemas.concat(buildRequestSchema(svc.name, method))
@@ -131,11 +127,14 @@ const buildRequestSchemasForFile = (schema: Schema): string => {
     return schemas
 }
 
-const buildResponseSchema = (svcName: string, method: MutationMethod | QueryMethod): string => {
-    const schema = ``
-
-    return `S.object().id('${svcName}.${method.name}Response').title('${svcName}.${method.name} Response').description('${svcName}.${method.name} Response Schema').prop('data', ${schema})`
-}
+const buildResponseSchema = (svcName: string, method: MutationMethod | QueryMethod): string =>
+    method.isVoidReturn
+        ? ''
+        : `const ${lowerCase(svcName)}${capitalize(method.name)} = S.object().id('${svcName}.${
+              method.name
+          }Response').title('${svcName}.${method.name} Response').description('${svcName}.${
+              method.name
+          } Response Schema').prop('data', ${schemaType(method.returnType)})`
 
 const buildResponseSchemasForFile = (schema: Schema): string => {
     let schemas = ''
@@ -151,10 +150,14 @@ const buildResponseSchemasForFile = (schema: Schema): string => {
     }
     return schemas
 }
-export const buildFluentSchemas = (schema: Schema): Code => {
-    const source = `
-  ${buildMsgSchemasForFile(schema)}
+
+export const requestSchemaName = (svcName: string, method: MutationMethod | QueryMethod): string =>
+    `${lowerCase(svcName)}${capitalize(method.name)}RequestSchema`
+
+export const responseSchemaName = (svcName: string, method: MutationMethod | QueryMethod): string =>
+    `${lowerCase(svcName)}${capitalize(method.name)}ResponseSchema`
+export const buildFluentSchemas = (schema: Schema): string =>
+    `${buildMsgSchemasForFile(schema)}
   ${buildRequestSchemasForFile(schema)}
-  ${buildResponseSchemasForFile(schema)}`
-    return { fileName: `${schema.fileName}.rpc.schemas.ts`, source }
-}
+  ${buildResponseSchemasForFile(schema)}
+  `
