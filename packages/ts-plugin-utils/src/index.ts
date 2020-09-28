da21c794fff17b22935a03adf1cf09638ba4c31a
Gary Becks <techstar.dev@hotmail.com>
2020-09-28 16:48:17 -0400

    make fluent schema functions a part of plugin-utils

--- packages/ts-plugin-utils/src/index.ts
+++ packages/ts-plugin-utils/src/index.ts
@@ -10,6 +10,7 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import { capitalize, lowerCase } from '@typerpc/plugin-utils'
 import {
     DataType,
     Import,
@@ -23,8 +24,7 @@ import {
     Schema,
     StructLiteralProp,
 } from '@typerpc/schema'
-import { capitalize, lowerCase } from '@typerpc/plugin-utils'
-
+import { buildFluentSchemas, requestSchemaName, responseSchemaName } from './fluent'
 export const typeMap: Map<string, string> = new Map<string, string>([
     [make.bool.type, 'boolean'],
     [make.int8.type, 'number'],
@@ -262,3 +262,5 @@ export const buildMsgImports = (imports: ReadonlyArray<Import>): string => {
     }
     return importsStr
 }
+
+export { buildFluentSchemas, requestSchemaName, responseSchemaName }
