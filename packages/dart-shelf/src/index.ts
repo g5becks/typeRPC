da1fbc45d15dbcc8f05fa4854f865b2436477220
Gary Becks <techstar.dev@hotmail.com>
2020-10-05 16:07:52 -0400

    small sketch of dart-shelf plugin

--- packages/dart-shelf/src/index.ts
+++ packages/dart-shelf/src/index.ts
@@ -1,5 +1,7 @@
+import { buildInterfaces, buildTypes } from '@typerpc/dart-plugin-utils'
 import { Code } from '@typerpc/plugin'
 import { Schema } from '@typerpc/schema'
+
 /*
  * Copyright (c) 2020. Gary Becks - <techstar.dev@hotmail.com>
  *
@@ -12,11 +14,16 @@ import { Schema } from '@typerpc/schema'
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-const buildFile = (schema: Schema): Code => {
-  let source = `
-  ${}`
+const fileName = (name: string): string => (name.includes('-') ? name.split('-').join('_') + '.dart' : name + '.dart')
 
-    return { fileName: '', source: '' }
+const buildFile = (schema: Schema): Code => {
+    const source = `
+    ${buildTypes(schema)}
+    ${buildInterfaces(schema)}
+
+  `
+
+    return { fileName: fileName(schema.fileName), source }
 }
 // builds all schemas and server file
 const build = (schemas: Schema[]): Code[] => [...schemas.map((schema) => buildFile(schema))]
